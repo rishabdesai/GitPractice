65a1050e2b1e88d3485fbf06b65fe18dfb4225cd
rishabhdesai <d_rishabh@hotmail.com>
2020-01-29 13:28:04 +0530

    first commit

---
+++ cpu_usage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import psutil
+
+def check_cup_usage(percent):
+    usage = psutil.cpu_percent()
+    return usage < percent
+
+if not check_cup_usage(75):
+    print("Overloaded CUP -- attention required")
+else:
+    print("everything is OK")
